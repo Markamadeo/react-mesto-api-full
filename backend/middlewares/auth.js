31c4800d435ddfaa1c7d90c08f684ceff9cc6840
Artem Kulakov <kulakov22@yandex.ru>
2021-02-26 22:37:09 +0700

    hotfix: fix crash-server

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,24 +4,15 @@ import jwt from 'jsonwebtoken';
 import UnauthorizedError from '../utils/errors/unauthorized-error.js';
 
 export const auth = (req, res, next) => {
-  const authorization = req.cookies.jwt;
-  if (!authorization) {
-    return;
-  }
-
-  const token = authorization;
-
+  const token = req.cookies.jwt;
   let payload;
-
   try {
     payload = jwt.verify(token, process.env.JWT_SECRET);
   } catch (e) {
     const err = new UnauthorizedError('Необходима авторизация');
     next(err);
   }
-
   req.user = payload;
-
   next();
 };
 
